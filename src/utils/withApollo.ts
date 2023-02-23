039268aa672ffc54a1633e8a5ebcc59207a6705d
Subigya Lamichhane <subigyalamichhane@gmail.com>
2023-02-23 09:57:10 +0545

    url changed

--- src/utils/withApollo.ts
+++ src/utils/withApollo.ts
@@ -4,8 +4,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 export const client = (ctx) =>
   new ApolloClient({
     // uri: process.env.GRAPHQL_SERVER as string,
-    // uri: process.env.NEXT_PUBLIC_API_URL,
-    uri: 'http://localhost:5000/graphql',
+    uri: process.env.NEXT_PUBLIC_API_URL,
+    // uri: '/graphql',
     credentials: 'include',
     headers: {
       cookie: ctx?.req?.headers?.cookie,
